b86f8ba583beaef7369a05c6e2a5a7108ad5a145
Arkaprabha Ghosh <arkag70@gmail.com>
2022-07-29 00:03:11 +0530

    fixed path issue

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 document.querySelector("button").onclick = function(){
     var player1 = Math.floor(Math.random() * 6) + 1;
     var player2 = Math.floor(Math.random() * 6) + 1;
-    var die_src_1 = document.getElementsByClassName("player1")[0].src.slice(8,75);
-    var die_src_2 = document.getElementsByClassName("player2")[0].src.slice(8,75);
-    document.getElementsByClassName("player1")[0].src = die_src_1 + player1 + ".png";
-    document.getElementsByClassName("player2")[0].src = die_src_2 + player2 + ".png";
+    document.getElementsByClassName("player1")[0].src = "data/dice" + player1 + ".png";
+    document.getElementsByClassName("player2")[0].src = "data/dice" + player2 + ".png";
 
     if(player1 > player2){
         document.querySelector("h1").innerText = "Player 1 wins";
